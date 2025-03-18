844cdaee040b53de151adf7a9d02f9b1d4ffc0b7
Bruno Veiga <b.veiga74@gmail.com>
2025-03-18 19:05:40 -0300

    dimmer update, simultaneo

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -1,6 +1,10 @@
+#include <Arduino.h>
+
 const int TRIG_PIN = 5;
-const int ECHO_PIN = 4;
+const int ECHO2_PIN = 12;
+const int ECHO1_PIN = 4;
 const int RED_LED = 10;      // the pin that the LED is attached to
+int loops = 0;
 
 void setup() {
   // initialize the serial communication:
@@ -8,30 +12,51 @@ void setup() {
 
   // initialize the PINS:
   pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(ECHO1_PIN, INPUT);
+  pinMode(ECHO2_PIN, INPUT);
   pinMode(RED_LED, OUTPUT);
 }
 
-long readUltrasonicDistance() {
+long readUltrasonicDistance1() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  return pulseIn(ECHO_PIN, HIGH);
+  int echo1 = pulseIn(ECHO1_PIN, HIGH);
+  
+  Serial.println("echo1: " + String(echo1));
+  return echo1;
 }
 
-
+long readUltrasonicDistance2() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  int echo2 = pulseIn(ECHO2_PIN, HIGH);
+  
+  Serial.println("echo2: " + String(echo2));
+  return echo2;
+}
+int delay_s = 1;
 void loop() {
   // Calcula distância uma única vez por loop
-  float distancia_cm = 0.01723 * readUltrasonicDistance();
-  byte brightness = 255 * max(0, min(70 - distancia_cm, 67)) / 67;
+  float distancia1_cm = 0.01723 * readUltrasonicDistance1();
+  delay(delay_s);
+  float distancia2_cm = 0.01723 * readUltrasonicDistance2();
+  distancia2_cm > 1 ? delay_s-- : delay_s++;
+  Serial.println("delay_s: " + String(delay_s));
+
+  byte brightness = 255 * max(0, min(70 - min(distancia1_cm, distancia2_cm), 67)) / 67;
   // Debug no Serial Monitor
-  Serial.println("Distância: " + String(distancia_cm) + " cm");
-  Serial.println("Brightness: " + String(brightness) + " cm");
-
+  Serial.println("Distância1: " + String(distancia1_cm) + " cm");
+  Serial.println("Distância2: " + String(distancia2_cm) + " cm");
+  Serial.println("Brightness: " + String(brightness));
   analogWrite(RED_LED, brightness);
+  loops += 1;
+  Serial.println("loops = " + String(loops));
 
-  delay(40);
+  delay(2000);
 }
-
